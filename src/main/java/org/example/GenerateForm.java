83124a66c482d65587fb972ee2e44fe5a493adf1
Vladimir Okhremenko <ohremenkovwork@gmail.com>
2024-04-25 18:40:46 +0300

    RABOTAET

--- src/main/java/org/example/GenerateForm.java
+++ src/main/java/org/example/GenerateForm.java
@@ -1,46 +1,75 @@
 package org.example;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.squareup.okhttp.*;
 import kong.unirest.Unirest;
 
+import java.awt.*;
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 public class GenerateForm {
 
-    private static final String PUBLIC_KEY = "YOUR API-KEY";
-    private static final String PRIVATE_KEY = "YOUR PRIVATE KEY";
-    private static final String BODY = "{\n  \"amount\": 500,\n  \"currencyID\": 3,\n  \"typeID\": 4,\n  \"clientOrderID\": \"129802376321867137613486734\",\n  \"TTL\": 999,\n  \"webhookURL\": \"https://webhook.site/fef4b9cf-c949-4533-9a33-8547911dabf1\",\n  \"extra\": {\n    \"comment\": \"payin for site x.com\",\n    \"allowedMethodIDs\": [\n      1,\n      2,\n      3,\n      4,\n      5,\n      6,\n      7,\n      8,\n      10\n    ],\n    \"failedRedirectURL\": \"http://youdomain.com/failed_redirect\",\n    \"successRedirectURL\": \"http://youdomain.com/success_redirect\",\n    \"payerInfo\": {\n      \"userAgent\": \"Mozilla/5.0 (iPhone; CPU iPhone OS 16_6_1 like Mac OS X) AppleWebKit/655.1.15 (KHTML, like Gecko) Version/16.4 Mobile/148 Safari/04.1\",\n      \"IP\": \"123.123.123.123\",\n      \"userID\": \"1\",\n      \"fingerprint\": \"fbb77b9f4265b18538e66cac5a37c6410dc2cdd7f0cddfde6eda25aa10df669b\",\n      \"registeredAt\": 1695057185\n    }\n  }\n}";
+    private static final String PUBLIC_KEY = "";
+    private static final String PRIVATE_KEY = "";
+    private static final String BODY =
+            """
+                    {
+                      "amount": 1000,
+                      "currencyID": 3,
+                      "typeID": 4,
+                      "clientOrderID": "1234161231421346153451",
+                      "TTL": 999,
+                      "webhookURL": "https://webhook.site/fef4b9cf-c949-4533-9a33-8547911dabf1",
+                      "extra": {
+                        "comment": "payin for site x.com",
+                        "allowedMethodIDs": [
+                          1,
+                          2,
+                          3,
+                          4,
+                          5,
+                          6,
+                          7,
+                          8,
+                          10
+                        ],
+                        "failedRedirectURL": "http://youdomain.com/failed_redirect",
+                        "successRedirectURL": "http://youdomain.com/success_redirect",
+                        "payerInfo": {
+                          "userAgent": "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6_1 like Mac OS X) AppleWebKit/655.1.15 (KHTML, like Gecko) Version/16.4 Mobile/148 Safari/04.1",
+                          "IP": "123.123.123.123",
+                          "userID": "1",
+                          "fingerprint": "fbb77b9f4265b18538e66cac5a37c6410dc2cdd7f0cddfde6eda25aa10df669b",
+                          "registeredAt": 1695057185
+                        }
+                      }
+                    }""";
 
-    public static void makeCon() throws NoSuchAlgorithmException, InvalidKeyException {
-        HttpClient client = HttpClient.newHttpClient();
-        Gson gson = new Gson();
+    public static void makeCon() throws NoSuchAlgorithmException, InvalidKeyException, IOException {
+        OkHttpClient client = new OkHttpClient();
 
-        String bodyJSON = gson.toJson(BODY);
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://api.aifory.io/payin/process"))
-                .header("Content-Type", "application/json")
-                .header("Signature", HMACUtil.signData(PRIVATE_KEY, bodyJSON))
-                .header("user-agent", "YOUR USER-AGENT")
-                .header("API-Key", PUBLIC_KEY)
-                .POST(HttpRequest.BodyPublishers.ofString(BODY))
+        MediaType mediaType = MediaType.parse("application/json");
+        RequestBody body = RequestBody.create(mediaType, BODY);
+        Request request = new Request.Builder()
+                .url("https://api.aifory.io/payin/process")
+                .post(body)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Signature", HMACUtil.signData(PRIVATE_KEY, BODY))
+                .addHeader("user-agent", "")
+                .addHeader("API-Key", PUBLIC_KEY)
                 .build();
-        System.out.println(bodyJSON);
-        CompletableFuture<HttpResponse<String>> responseFuture = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
 
+        Response response = client.newCall(request).execute();
 
-        responseFuture.thenAccept(response -> {
-            System.out.println(response.statusCode());
-            System.out.println(response.body());
-        });
-
-
-        responseFuture.join();
+        System.out.println(response.body().string());
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,13 @@ package org.example;
 
 import kong.unirest.Unirest;
 
+import java.io.IOException;
 import java.net.http.HttpResponse;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
 public class Main {
-    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException, IOException {
         GenerateForm.makeCon();
     }
 }
